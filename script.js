cd17fdfb7a6bfadc9c2e233274ad72432b144d19
DoCodingkeval <gohelkeval7@gmail.com>
2025-07-22 15:36:04 +0530

    commit

--- script.js
+++ script.js
@@ -26,60 +26,91 @@ const navbar = () => {
 
 window.addEventListener("scroll", navbar);
 
-gsap.from(".img-1", 2, {
-  opacity: 0,
-  x: 400,
-});
-gsap.from(".img-2", 1, {
-  opacity: 0,
-  y: 400,
-  delay: 0.5,
-  ease: "back-out(1.5)",
-});
-gsap.from(".img-3", 1, {
-  opacity: 0,
-  y: 400,
-  delay: 0.2,
-  ease: "back-out(1.5)",
-});
-gsap.from(".img-4", 1, {
-  opacity: 0,
-  y: 200,
-  delay: 1,
-  ease: "back-out(1.5)",
-});
-gsap.from(".img-5", 1, {
-  opacity: 0,
-  y: 400,
-  delay: 0.2,
-  ease: "back-out(1.5)",
-});
-gsap.from(".img-6", 1, {
-  opacity: 0,
-  y: 200,
-  delay: 0.1,
-  ease: "back-out(1.5)",
-});
+// =============GSAP Animation====================
 
-gsap.from(".temple__details", 1.2, {
-  opacity: 0,
-  y: -100,
-  delay: 1.5,
-  ease: "back-out(1.5)",
-});
+const GSAPAnimations = () => {
+  // ==========Links animation==============
+  gsap.from(".nav li", 0.9, {
+    opacity: 0,
+    y: 100,
+    delay: 0.4,
+    ease: "back.out(1)",
+    stagger: 0.3,
+  });
+  // ==========Logo animation===============
+  gsap.from(".nav__logo", 1, {
+    opacity: 0,
+    x: -50,
+    delay: 0.4,
+    ease: "back.out(1)",
+  });
+  // =============Bg images animation==============
+  gsap.from(".img-1", 1, {
+    opacity: 0,
+    x: 400,
+    delay: 0.5,
+    ease: "power3.out",
+  });
 
-gsap.from(".lantern-1", 1.2, {
-  opacity: 0,
-  x: 70,
-  delay: 1.8,
-  ease: "back-out(1.5)",
-});
+  gsap.from(".img-2", 1, {
+    opacity: 0,
+    y: 400,
+    delay: 0.7,
+    ease: "power3.out",
+  });
 
-gsap.from(".lantern-2", 1.2, {
-  opacity: 0,
-  x: -70,
-  delay: 2,
-  ease: "back-out(1.5)",
-});
+  gsap.from(".img-3", 1, {
+    opacity: 0,
+    y: 400,
+    delay: 0.4,
+    ease: "power3.out",
+  });
+
+  gsap.from(".img-4", 1, {
+    opacity: 0,
+    y: 400,
+    delay: 1,
+    ease: "power3.out",
+  });
+
+  gsap.from(".img-5", 1, {
+    opacity: 0,
+    y: 400,
+    delay: 0.5,
+    ease: "power3.out",
+  });
+
+  gsap.from(".img-6", 1, {
+    opacity: 0,
+    y: 400,
+    delay: 0.1,
+    ease: "power3.out",
+  });
+
+  gsap.from(".temple__details", 1, {
+    opacity: 0,
+    x: -150,
+    delay: 0.8,
+    ease: "power1.out",
+  });
+
+  gsap.from(".lantern-1", 1, {
+    opacity: 0,
+    y: -40,
+    delay: 1,
+    ease: "power1.out",
+  });
+
+  gsap.from(".lantern-2", 1, {
+    opacity: 0,
+    y: 40,
+    delay: 1,
+    ease: "power1.out",
+  });
+};
+
+GSAPAnimations();
+
+// ============Sakura Petals=============
 
 const sakura = new Sakura(".sakura-petals");
